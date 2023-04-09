055a9147c1fd0fc4be9f363de9411894fe3e0ef0
carlafaes <carlafaes18@hotmail.com>
2023-04-09 18:26:32 -0300

    se agrega else y finally

--- primerClase/manejo_excepciones3.py
+++ primerClase/manejo_excepciones3.py
@@ -9,6 +9,10 @@ except ZeroDivisionError as e:
     print(f'ZeroDivisionError-Ocurrio un error: {type(e)}')
 except Exception as e:
     print(f'Exception-Ocurrio un error: {type(e)}')
+else:
+    print("No se arrojo ninguna excepcion")
+finally:#Siempre se va a ejecutar
+    print("Ejecucion de este bloque finally")
 
 print(f'El resultado es: {resultado}')
 print('seguimos...')
