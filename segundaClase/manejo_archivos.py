6a42bd06fcf6b0290d39cde232d59fc43f0a60a3
carlafaes <carlafaes18@hotmail.com>
2023-04-10 20:05:08 -0300

    manejo de archivos

--- segundaClase/manejo_archivos.py
+++ segundaClase/manejo_archivos.py
@@ -1,6 +1,8 @@
 #declaramos una variable
 try:
     archivo=open('prueba.txt','w')#la w es de write
+    archivo.write('Programamos con diferentes tipos de archivos, ahora en txt.\n')
+    archivo.write('Con esto terminamos')
 except Exception as e:
     print(e)
 finally:
