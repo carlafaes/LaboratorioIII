2f95b13a7ee0d692759b4436ea494e4dcb66f63f
carlafaes <carlafaes18@hotmail.com>
2023-04-10 20:46:18 -0300

    agrega readline

--- segundaClase/leer_archivos.py
+++ segundaClase/leer_archivos.py
@@ -1,2 +1,4 @@
 archivo=open('prueba.txt', 'r',encoding='utf-8')#r es de read, a es de append, w es de write, x crea un archivo,t especifica el tipo de archivo (texto o text), b tipo de archivo binario, w+ se utiliza para leer y tambien escribir informacion,r+ para escribir y leer informacion
-print(archivo.read())
+#print(archivo.read())
+#print(archivo.read(5))#muestra las 5 primeras letras de la primer palabra
+print(archivo.readline())# lee la linea completa
