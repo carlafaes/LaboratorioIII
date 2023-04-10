8d64bf4173d5b52dea44dd812a193dd4b8b1d43f
carlafaes <carlafaes18@hotmail.com>
2023-04-10 20:27:01 -0300

    encoding utf-8

--- segundaClase/manejo_archivos.py
+++ segundaClase/manejo_archivos.py
@@ -1,7 +1,9 @@
 #declaramos una variable
 try:
-    archivo=open('prueba.txt','w')#la w es de write
+    archivo=open('prueba.txt','w', encoding='utf-8')#la w es de write
     archivo.write('Programamos con diferentes tipos de archivos, ahora en txt.\n')
+    archivo.write('Los acentos son importantes para las palabras\n')
+    archivo.write('como por ejemplo: acción,ejecución y producción \n')
     archivo.write('Con esto terminamos')
 except Exception as e:
     print(e)
